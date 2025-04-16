dd83c4472dbd96545a8bf1f08a4cc141438a380b
Your Name <your_email@example.com>
2025-04-16 15:37:12 +0530

    change in login file

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
         try {
             event.preventDefault();
 
-            const {data} = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/user/${state}`,{
+            const {data} = await axios.post(`${import.meta.env.VITE_API_BASE_URL}/api/user/login`,{
                 name, email, password
             });
             if (data.success){
